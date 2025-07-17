c760ce8cc7a1c74d932b07cc66c7c58a28463c8e
Rayhan Rahman <rayhanhussain12@gmail.com>
2025-07-17 00:13:25 -0400

    removed white spaces

--- src/main/java/baseutil/Baseclass.java
+++ src/main/java/baseutil/Baseclass.java
@@ -1,67 +1,41 @@
 package baseutil;
 
 import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-
 import commonAction.CommonAction;
 import pages.LoginPage;
-
+// Removed white spaces
 public class Baseclass {
-	
+
 	public WebDriver driver;
 	public LoginPage login;
 	public CommonAction ca;
-	
-  
+
 	@BeforeClass
-	
 	public void setup() {
-		
-	driver = new ChromeDriver();
-	driver.manage().window().maximize();
-	driver.get("https://enthrallit.com/accounts/login/");
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	allClass();
-	
-	
-	}
-	
-	public void allClass() {
-		
-		login = new LoginPage(driver);
-		ca= new CommonAction();
-		
-		
-		
-		
-		
-		
-		
-	}
-	
-	@AfterClass
-	
-	public void teardown() {
-		driver.quit();
-		
-	}
-	
-	public void over() {
-		driver.findElement(By.id("abc"));
-		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://enthrallit.com/accounts/login/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		allClass();
 	}
 
-	
-	
-	
-	
-	
-	
-	
+	public void allClass() {
+		login = new LoginPage(driver);
+		ca = new CommonAction();
+	}
+
+	@AfterClass
+	public void teardown() {
+		driver.quit();
+	}
+
+	public void over() {
+		driver.findElement(By.id("abc"));
+	}
 
 }
